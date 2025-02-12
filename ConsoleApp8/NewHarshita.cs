dc6cc38ca8e5fce69dfd5a95d2c159a471c84ab4
Harshita-30034719 <harshita.b.jain@capgemini.com>
2025-02-12 13:53:54 +0530

    State property

---
+++ ConsoleApp8/NewHarshita.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    internal class NewHarshita
+    {
+        public string State { get; set; }
+    }
+}
